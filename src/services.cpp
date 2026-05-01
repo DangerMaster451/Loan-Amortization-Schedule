2c1a31b96951c8fbb4fd472901148d159bee376e
Andrew <andrew.r.pelton@gmail.com>
2026-04-30 17:43:03 -0700

    Can update the webpage

--- src/services.cpp
+++ src/services.cpp
@@ -4,59 +4,88 @@
 #include <vector>
 #include <fstream>
 
-namespace CSVService {
-    void generateFile(std::vector<Payment*> payments, std::string fileName)
-    {
-        std::fstream fout;
+void CSVService::generateFile(std::vector<Payment*> payments, std::string fileName)
+{
+    std::fstream fout;
 
-        fout.open(fileName, std::ios::out);
+    fout.open(fileName, std::ios::out);
 
-        for (Payment* p : payments) {
-            fout << p->toCSVRow() << std::endl;
-        }
+    for (Payment* p : payments) {
+        fout << p->toCSVRow() << std::endl;
     }
 }
+
 
 namespace HTMLService {
 
 }
 
-namespace ServerService {
-
-    std::string buildPage() {
-        return R"(
-            <!DOCTYPE html>
-            <html><body>
-            <p>Loan Amortization App</p>
-            <form action="/add" method="POST">
-                <label for="principal">Principal:</label><br>
-                <input type="number" id="principal" name="principal"><br>
-                <label for="term">Term (Years):</label><br>
-                <input type="number" id="term" name="term"><br>
-                <label for="APR">APR:</label><br>
-                <input type="number" id="APR" name="APR" min=0 max=100 step=0.01><br>
-                <input type="submit" value="Submit">
-            </form>
-            </body></html>
-        )";
-    }
-
-    void run() {
-        httplib::Server svr;
-
-        svr.Get("/", [](const httplib::Request&, httplib::Response& res) {
-            res.set_content(ServerService::buildPage(), "text/html");
-        });
-
-        svr.Post("/add", [](const httplib::Request& req, httplib::Response& res) {
-            std::cout << "Principal: " << req.get_param_value("principal") << std::endl;
-            std::cout << "Term: "      << req.get_param_value("term")      << std::endl;
-            std::cout << "APR: "       << req.get_param_value("APR")       << std::endl;
-
-            res.set_redirect("/");
-        });
-
-        svr.listen("0.0.0.0", 8080);
-    }
+ServerService::UserInput::UserInput(float principal, int term, float APR) {
+    this->principal = principal;
+    this->term = term;
+    this->APR = APR;
 }
 
+std::string ServerService::buildTable(UserInput* input)
+{
+    if (input == nullptr)
+    {
+        return R"(
+        <p>Nothing here yet :)</p>
+    )";
+    }
+    return R"(
+    <table>
+        <tr>
+            <tc>Payment #</tc>
+            <tc>Payment $</tc>
+            <tc>Principal</tc>
+            <tc>Interest</tc>
+            <tc>Balence</tc>
+        </tr>
+    </table>
+
+    )";
+}
+
+std::string ServerService::buildPage(UserInput* input) {
+    std::string table = buildTable(input);
+    return R"(
+        <!DOCTYPE html>
+        <html><body>
+        <p>Loan Amortization App</p>
+        <form action="/add" method="POST">
+            <label for="principal">Principal:</label><br>
+            <input type="number" id="principal" name="principal"><br>
+            <label for="term">Term (Years):</label><br>
+            <input type="number" id="term" name="term"><br>
+            <label for="APR">APR:</label><br>
+            <input type="number" id="APR" name="APR" min=0 max=100 step=0.01><br>
+            <input type="submit" value="Submit">
+        </form>
+        <table>)" + 
+            table + 
+        R"(</table>
+        </body></html>
+    )";
+}
+
+void ServerService::run() {
+    httplib::Server svr;
+    UserInput* input = nullptr;
+
+    svr.Get("/", [&input](const httplib::Request&, httplib::Response& res) {
+        res.set_content(ServerService::buildPage(input), "text/html");
+    });
+
+    svr.Post("/add", [&input](const httplib::Request& req, httplib::Response& res) {
+        float principal = stof(req.get_param_value("principal"));
+        int term = stof(req.get_param_value("term"));
+        float APR = stof(req.get_param_value("APR"));
+        res.set_redirect("/");
+
+        input = new UserInput(principal, term, APR);
+    });
+
+    svr.listen("0.0.0.0", 8080);
+}
--- src/services.h
+++ src/services.h
@@ -13,5 +13,15 @@ namespace HTMLService {
 }
 
 namespace ServerService {
+    struct UserInput {
+        float principal;
+        int term;
+        float APR;
+        UserInput() = default;
+        UserInput(float principal, int term, float APR);
+    };
+
     void run();
+    std::string buildPage(UserInput* input);
+    std::string buildTable(UserInput* input);   
 }
